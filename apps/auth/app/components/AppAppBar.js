2e1ddad3627bebaea6903bd8179ab02bc9591133
Cristian <cristian.sulbaran.r@gmail.com>
2024-05-16 00:20:58 -0300

    se añadió un layout con un componente copiado de un repositorio

--- apps/auth/app/components/AppAppBar.js
+++ apps/auth/app/components/AppAppBar.js
@@ -34,15 +34,16 @@ function AppAppBar({ mode, toggleColorMode }) {
   const scrollToSection = (sectionId) => {
     const sectionElement = document.getElementById(sectionId);
     const offset = 128;
-    if (sectionElement) {
-      const targetScroll = sectionElement.offsetTop - offset;
-      sectionElement.scrollIntoView({ behavior: "smooth" });
-      window.scrollTo({
-        top: targetScroll,
-        behavior: "smooth",
-      });
-      setOpen(false);
+    if (!sectionElement) {
+      return;
     }
+    const targetScroll = sectionElement.offsetTop - offset;
+    sectionElement.scrollIntoView({ behavior: "smooth" });
+    window.scrollTo({
+      top: targetScroll,
+      behavior: "smooth",
+    });
+    setOpen(false);
   };
 
   return (
